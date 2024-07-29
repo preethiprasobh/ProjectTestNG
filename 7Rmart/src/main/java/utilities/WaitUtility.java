aa26970cb6e72cef6afd0a27c7300bbd7d289ac4
PreethiArun <160821896+preethiprasobh@users.noreply.github.com>
2024-07-29 11:58:18 +0530

    hello

--- 7Rmart/src/main/java/utilities/WaitUtility.java
+++ 7Rmart/src/main/java/utilities/WaitUtility.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class WaitUtility {
 	public static final long EXPLICIT_WAIT = 10;
 	public static final long IMPLICIT_WAIT=10;
+	
 	public void waitForElement(WebDriver driver, WebElement target) {
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(EXPLICIT_WAIT));
 		wait.until(ExpectedConditions.visibilityOf(target));
@@ -17,12 +18,12 @@ public class WaitUtility {
 		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(EXPLICIT_WAIT));
 	    wait.until(ExpectedConditions.elementToBeClickable(element));
 	}
-public void waitForAlertToBeVisible(WebDriver driver) {
+	public void waitForAlertToBeVisible(WebDriver driver) {
 		    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(EXPLICIT_WAIT));
 		    wait.until(ExpectedConditions.alertIsPresent());
 	}
 
-public void waitForElementToBeSelected(WebDriver driver,WebElement element) {
+	public void waitForElementToBeSelected(WebDriver driver,WebElement element) {
 	 WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(EXPLICIT_WAIT));
 	 wait.until(ExpectedConditions.elementToBeSelected(element));
 }
